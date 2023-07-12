7b25f4294f7ad9cde463eafbc7216650b719238c
Abraham Salazar <abraham_jack10@hotmail.com>
2023-07-12 12:06:12 -0500

    build: :heavy_plus_sign: Install Ngrx Effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 
-const COMMON = [BrowserModule, AppRoutingModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})];
+const COMMON = [
+  BrowserModule,
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  StoreModule.forRoot({}, {}),
+  EffectsModule.forRoot([]),
+];
 const UTILS = [LayoutModule];
 const PAGES = [LandingComponent];
-const COMPONENTS = [];
 
 @NgModule({
   declarations: [AppComponent],
