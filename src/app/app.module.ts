3fed3c31ee1cd71e5c2957986c01af33444aaca0
Abraham Salazar <abraham_jack10@hotmail.com>
2023-07-12 12:01:02 -0500

    build: :heavy_plus_sign: Install Ngrx Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 
-const COMMON = [BrowserModule, AppRoutingModule, BrowserAnimationsModule];
+const COMMON = [BrowserModule, AppRoutingModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})];
 const UTILS = [LayoutModule];
 const PAGES = [LandingComponent];
 const COMPONENTS = [];
