90695d7394978ce391028c78f066266d97579348
Abraham Salazar <abraham_jack10@hotmail.com>
2023-07-12 12:10:05 -0500

    build: :heavy_plus_sign: Install Ngrx Store Dev Tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { LayoutModule } from '@angular/cdk/layout';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
@@ -15,6 +16,7 @@ const COMMON = [
   BrowserAnimationsModule,
   StoreModule.forRoot({}, {}),
   EffectsModule.forRoot([]),
+  StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode(), autoPause: true }),
 ];
 const UTILS = [LayoutModule];
 const PAGES = [LandingComponent];
